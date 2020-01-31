68be8ae2d6f4a56c6a22b8b58e84048201899445
Joel Lacey <joellaceybusiness@gmail.com>
2020-01-31 12:48:04 -0700

    Build basic ContactDash wireframe

---
+++ src/ContactDash/ContactDash.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ContactDash = () => {
+    return (
+        <main className="ContactDash">
+            <h2>To contact this week:</h2>
+            <section className="WeekContact">
+            </section>
+            {/* add birthday section in fucture */}
+        </main>
+    )
+}
+
+export default ContactDash;
