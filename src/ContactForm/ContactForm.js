1111a59efdf315198097ed2015ca3934ad61dbf4
Joel Lacey <joellaceybusiness@gmail.com>
2020-02-12 16:48:42 -0700

    Update form styling and structure to grid basis

--- src/ContactForm/ContactForm.js
+++ src/ContactForm/ContactForm.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import "./ContactForm.scss";
+import { NavLink } from 'react-router-dom';
+
 export default class ContactForm extends Component {
   constructor() {
       super()
@@ -11,25 +14,36 @@ export default class ContactForm extends Component {
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value})
   }
+
+  captureContact = e => {
+    e.preventDefault();
+    this.props.addContact(this.state);
+  }
   
   render() {
     return (
-      <form className="ContactForm">
-        <input
-          type="text"
-          name="name"
-          placeholder="Contact name"
-          value={this.state.name}
-          onChange={this.handleChange}
-        />
-        <input
-          type="number"
-          name="number"
-          placeholder="Contact number"
-          value={this.state.number}
-          onChange={this.handleChange}
-        />
+      <form className="contact-form">
+        <h1>Add a contact.</h1>
+        <h3>What have you been chatting about?</h3>
+        <section className="input-cont">
+          <input
+            type="text"
+            name="name"
+            placeholder="Contact name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            name="number"
+            placeholder="Contact number"
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+        </section>
+        <NavLink to='/contacts' className='add-contact'>Add Contact</NavLink>
       </form>
     );
   } 
 }
+// onClick = { this.captureContact }
