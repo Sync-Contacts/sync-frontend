075cf81333080c9732dc304854a84f56c5bbd999
Joel Lacey <joellaceybusiness@gmail.com>
2020-05-19 18:17:41 -0600

    Convert Contact to typescript add styling

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import userImg from "../resource/user.svg"
+import userImg from "../resource/user.svg";
 import "./Contact.scss";
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
   return (
     <section className='contact-overflow'>
-      <section className="contact">
+      <Link to="/contactDetails" className="contact">
         <img src={userImg} alt="A contact"></img>
         <h3>Name</h3>
         <h3>(111) 222-3333</h3>
-      </section>
+      </Link>
     </section>
   );
 };
