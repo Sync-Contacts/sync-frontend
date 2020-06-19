a410788faa405d0789e1ce25185a61f3a1393725
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-18 22:09:06 -0600

    Add contactDash state integration for fetch

--- src/ContactDash/ContactDash.tsx
+++ src/ContactDash/ContactDash.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Contact from '../Contact/Contact'
 import "./ContactDash.scss";
 
-type ContactDashProps = {
-  
-}
-
 const ContactDash = () => {
+  //should type this more strictly for each property 
+    interface ContactState {
+      contacts: object[];
+    }
+
+  const [contacts, setContacts] = useState<ContactState | undefined>(undefined);
+
     return (
       <main className="contact-dash">
         <h2>To contact this week:</h2>
