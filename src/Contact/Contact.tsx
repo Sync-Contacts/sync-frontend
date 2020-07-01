07fcc6daca7195ab96290a4326412cc15410f0e7
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-30 20:19:18 -0600

    Add initial props interface to ContactProps

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -3,13 +3,19 @@ import "./Contact.scss";
 import userImg from '../resource/user.svg';
 import { Link } from 'react-router-dom';
 
-const Contact = () => {
+interface ContactProps {
+  id: number,
+  name: string,
+  number: string
+}
+
+const Contact: React.SFC<ContactProps> = ({id, name, number}) => {
   return (
     <section className='contact-overflow'>
       <Link to="/contactDetails" className="contact">
         <img src={userImg} alt="A contact"></img>
-        <h3>Name</h3>
-        <h3>(111) 222-3333</h3>
+        <h3>{name}</h3>
+        <h3>{number}</h3>
       </Link>
     </section>
   );
