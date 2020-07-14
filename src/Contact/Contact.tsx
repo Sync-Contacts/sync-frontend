249669ee0c06a12f71334bb0dec8dfd6c3e35b7d
Joel Lacey <joellaceybusiness@gmail.com>
2020-07-13 21:26:35 -0400

    Add an interface for contact mapping

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -3,19 +3,28 @@ import "./Contact.scss";
 import userImg from '../resource/user.svg';
 import { Link } from 'react-router-dom';
 
-interface ContactProps {
+interface ContactDetails {
   id: number,
-  name: string,
-  number: string
+  linked_user_id: number,
+  contact_name: string,
+  phone: string,
+  email: string,
+  contact_address: string,
+  last_contact: string,
+  contact_frq: number
 }
 
-const Contact: React.SFC<ContactProps> = ({id, name, number}) => {
+interface ContactProps {
+  contact_details: ContactDetails[];
+}
+
+const Contact: React.SFC<ContactProps> = ({ id, contact_name, phone }) => {
   return (
     <section className='contact-overflow'>
       <Link to="/contactDetails" className="contact">
         <img src={userImg} alt="A contact"></img>
-        <h3>{name}</h3>
-        <h3>{number}</h3>
+        <h3>{contact_name}</h3>
+        <h3>{phone}</h3>
       </Link>
     </section>
   );
